ed302dbd06c547137bb4fe5a23b81d5d2354bdc9
Moritz Reinert// Laptop <mr148@hdm-stuttgart.de>
2017-01-21 00:37:35 +0100

    fps pickup

---
+++ ccGameJam/Assets/Scripts/interactableScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactableScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void getPickedUp(Vector3 position)
+    {
+        transform.position = position;
+    }
+}
