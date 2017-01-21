39bfd0311e829ebd38a456808f95dd90b0334c28
Moritz Reinert// Laptop <mr148@hdm-stuttgart.de>
2017-01-21 22:33:38 +0100

    comitt

---
+++ ccGameJam/Assets/Scripts/startingScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startingScript : MonoBehaviour {
+
+    [SerializeField]
+    Material[] signalMats;
+    int nextMat = 0;
+    [SerializeField]
+    Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (GetComponent<Animation>().isPlaying)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+        else { GetComponent<MeshRenderer>().enabled = false; }
+
+
+
+	}
+
+    public void startAnim()
+    {
+
+        anim.SetTrigger("start");
+        Invoke("changeToLowSignal", 3f);
+    }
+
+    void changeToLowSignal()
+    {
+        GetComponent<Renderer>().material = signalMats[nextMat];
+        nextMat += 1;
+        if(nextMat <=4)
+        Invoke("changeToLowSignal", 0.8f);
+
+  
+    }
+}
---
+++ ccGameJam/Assets/startingManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startingManager : MonoBehaviour {
+
+    GameObject playerObject;
+    GameObject smartphoneObject;
+
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject.FindGameObjectWithTag("TopDownCam").SetActive(false);
+        Camera.main.enabled = true;
+
+        GameObject.FindGameObjectWithTag("smartphone").GetComponent<startingScript>().startAnim();
+        GameObject.FindGameObjectWithTag("smartphone").transform.parent.GetChild(1).GetComponent<Renderer>().enabled = true; 
+
+        Invoke("startAnim", 5f);
+		
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void startAnim()
+    {
+        Camera.main.GetComponent<Animation>().Play("headAnim");
+    }
+}
