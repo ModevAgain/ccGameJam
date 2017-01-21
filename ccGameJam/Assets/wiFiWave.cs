68b137e4c0b39d5f697446847b06107d8d2efea0
Nico <ns088@hdm-stuttgart.de>
2017-01-21 02:20:25 +0100

    WiFi Respawn, WiFi Movement

--- ccGameJam/Assets/wiFiWave.cs
+++ ccGameJam/Assets/wiFiWave.cs
@@ -6,27 +6,48 @@ public class wiFiWave : MonoBehaviour {
 
     [SerializeField]
     public GameObject forwardObj;
+    [SerializeField]
+    float transparancy;
+    Color color;
+    Material tempMaterial;
+    Material transparentMaterial;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    GameObject child;
 
     // Use this for initialization
-	void Start () {
-        transform.position = forwardObj.transform.position;
+    void Start () {
+
+        transform.position = transform.parent.transform.position;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
+        child = transform.GetChild(0).gameObject;
+        transform.position = Vector3.Lerp(transform.position, transform.GetChild(0).transform.position, speed);
 
-        
-		
 	}
     
 
 
     void OnCollisionEnter (Collision other)
     {
-        Debug.Log("haaaallo");
+        
         if(other.gameObject.tag == "outsideWall")
         {
             Destroy(gameObject);
+        }
+
+        if(other.gameObject.tag == "insideWall")
+        {
+
+            color = GetComponent<Renderer>().material.color;
+            Debug.Log(color);
+            color.a -= transparancy;
+            GetComponent<Renderer>().material.color = color;
+            Debug.Log(color);
         }
     }
 }
