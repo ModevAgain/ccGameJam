fd3c71383daf0bb8987029786202ce6e7b050e71
Nico <ns088@hdm-stuttgart.de>
2017-01-21 21:58:19 +0100

    Socket Position / Snap?!?!?!?!?!?! AAAAHHH

--- ccGameJam/Assets/Scripts/interactableScript.cs
+++ ccGameJam/Assets/Scripts/interactableScript.cs
@@ -6,28 +6,48 @@ public class interactableScript : MonoBehaviour {
 
     router rout;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rout = GetComponent<router>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            releaseObject();
+        }       
+    }    
+
+    public void releaseObject()
+    {
+        transform.parent = null;
+        gameObject.AddComponent<Rigidbody>();
+        //GetComponent<Rigidbody>().useGravity = true;
+        //GetComponent<Rigidbody>().isKinematic = false;
+    }
 
     public void getPickedUp(Vector3 position)
     {
+        Destroy(GetComponent<Rigidbody>());
         transform.position = position;
     }
 
     public void activateRouter()
-    {
-        
+    {        
         rout.routerActive = true;
-        //TODO switch to TopDown
+        //TODO switch to TopDown   
 
-        
-
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "socket")
+        {
+            Destroy(gameObject.GetComponent<Rigidbody>());
+            transform.parent = other.transform.parent;
+            transform.position = other.transform.position;
+        }
     }
 }
